ccbf5f66f88f66e98bfa2d566524cd5636b5d8ee
SiemV <swl.vermeulen@student.avans.nl>
2024-03-19 21:26:23 +0100

    Add files from Siem

---
+++ ui/script.js
@@ -0,0 +1,15 @@
+// Initialize a counter to keep track of the row number
+var rowCount = 1;
+
+// JavaScript for adding a row dynamically at the top
+document.getElementById("addRowBtn").addEventListener("click", function() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(1); // Insert row at the top of the table
+    var cell = row.insertCell(0); // Insert cell into the row
+    cell.innerHTML = "Hallo siem ik ben dit aan het testen. er blijven maar 10 statussen in deze tabel " + rowCount++; // Add content to the cell with incremented counter
+    var size = table.rows.length;
+    var lastRowIndex = table.rows.length - 1; // Index of the last row
+    if (size > 11) { // Check if there is more than one row (excluding the header row)
+        table.deleteRow(lastRowIndex); // Delete the last row
+    }
+});
