656b3af4e3b13d6c810045345984783499bf29f9
lonkaars <loek@pipeframe.xyz>
2024-04-01 14:19:53 +0200

    use single variable for message type field

--- ui/js/socket.js
+++ ui/js/socket.js
@@ -1,5 +1,7 @@
 import api from './api.js';
 
+const msgTypeKey = "functie";
+
 var ws = new WebSocket('ws://localhost:8081/');
 export default ws;
 
@@ -7,14 +9,14 @@ ws.addEventListener("message", ev => {
 	// this will already throw an error if the message doesn't contain valid JSON
 	const msg = JSON.parse(ev.data);
 
-	// check if api.msg.handle has a handler for msg.type
-	if (!api.msg.handle.hasOwnProperty(msg.type)) {
-		console.warn(`No message handler for type ${msg.type}`, msg);
+	// check if api.msg.handle has a handler for message type
+	if (!api.msg.handle.hasOwnProperty(msg[msgTypeKey])) {
+		console.warn(`No message handler for type ${msg[msgTypeKey]}`, msg);
 		return;
 	}
 
 	// run the appropriate message handler
-	api.msg.handle[msg.type](msg);
+	api.msg.handle[msg[msgTypeKey]](msg);
 });
 
 ws.addEventListener("close", () => {
